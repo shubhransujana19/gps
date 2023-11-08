31d23c490085944b24ec8b33d5b05cd5d64195ae
Shubhransu Jana <91710462+shubhransujana19@users.noreply.github.com>
2023-11-08 17:20:13 +0530

    Update server.php

--- server.php
+++ server.php
@@ -1,12 +1,14 @@
+<?php  mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); try { $con = new mysqli('localhost',  'admin', '', 'employee_tracking'); $con->set_charset('utf8mb4');} catch(Exception $e) { error_log($e->getMessage());}  ?>
+
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Extract and validate the received data (latitude, longitude, timestamp, mapping status, client name)
-    $latitude = $_POST['latitude'];
-    $longitude = $_POST['longitude'];
-    $timestamp = $_POST['timestamp'];
-    $address = $_POST['address'];
-    $mappingStatus = $_POST['mappingStatus'];
-    $clientName = $_POST['clientName'];
+    // Extract and validate the received data (latitude, longitude, timestamp, address, mapping status, client name)
+    $latitude = isset($_POST['latitude']) ? $_POST['latitude'] : null;
+    $longitude = isset($_POST['longitude']) ? $_POST['longitude'] : null;
+    $timestamp = isset($_POST['timestamp']) ? $_POST['timestamp'] : null;
+    $address = isset($_POST['address']) ? $_POST['address'] : null;
+    $mappingStatus = isset($_POST['mappingStatus']) ? $_POST['mappingStatus'] : null;
+    $clientName = isset($_POST['clientName']) ? $_POST['clientName'] : null;
 
     // Get the current date in the format YYYY-MM-DD
     $currentDate = date('Y-m-d');
@@ -18,20 +20,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         die("Connection failed: " . $connection->connect_error);
     }
 
-    // Check if there is an existing record for the client and date
-    $existingRecord = $connection->query("SELECT * FROM tracking_data WHERE client_name = '$clientName' AND date = '$currentDate'");
+    // Prepare the SQL statement
+    $sql = "INSERT INTO tracking_data (latitude, longitude, timestamp, address, mapping_status, client_name, date)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+            ON DUPLICATE KEY UPDATE
+            latitude = VALUES(latitude),
+            longitude = VALUES(longitude),
+            timestamp = VALUES(timestamp),
+            address = VALUES(address),
+            mapping_status = VALUES(mapping_status)";
 
-    if ($existingRecord->num_rows > 0) {
-        // Update the existing record
-        $sql = "UPDATE tracking_data SET latitude = ?, longitude = ?, timestamp = ?, address = ?, mapping_status = ? WHERE client_name = ? AND date = ?";
-        $stmt = $connection->prepare($sql);
-        $stmt->bind_param("ddssss", $latitude, $longitude, $timestamp, $address, $mappingStatus, $clientName, $currentDate);
-    } else {
-        // Insert a new record
-        $sql = "INSERT INTO tracking_data (latitude, longitude, timestamp, address, mapping_status, client_name, date) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $connection->prepare($sql);
-        $stmt->bind_param("ddsssss", $latitude, $longitude, $timestamp, $address, $mappingStatus, $clientName, $currentDate);
-    }
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param("ddsssss", $latitude, $longitude, $timestamp, $address, $mappingStatus, $clientName, $currentDate);
 
     if ($stmt->execute()) {
         echo "Data received and stored successfully.";
